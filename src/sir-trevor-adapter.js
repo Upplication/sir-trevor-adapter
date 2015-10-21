cd6f6da0c52dd54f9ac12f377e5917c508719622
Souler <juanjo.herrero@upplication.com>
2015-10-21 12:02:28 +0200

    Dont wrap the html with st-render-container when it wasnt an array

--- src/sir-trevor-adapter.js
+++ src/sir-trevor-adapter.js
@@ -169,10 +169,17 @@
      * @requires lodash
      */
     SirTrevorAdapter.prototype.toHTML = function(json) {
-        if (!_.isArray(json))
+        var wasArray = true;
+        if (!_.isArray(json)) {
             json = [ json ];
+            wasArray = false;
+        }
+
         var container = $('<' + this.config.elementEnclosingTag + '>', { class: this.config.elementClass + '-container' });
-        return '<div class="st-render-container">' + json.map(this.map, this).join('\n') + '</div>';
+        if (wasArray)
+            return '<div class="st-render-container">' + json.map(this.map, this).join('\n') + '</div>';
+        else
+            json.map(this.map, this).join('\n'):
     }
     SirTrevorAdapter.prototype.fromJSON = SirTrevorAdapter.prototype.toHTML;
 
