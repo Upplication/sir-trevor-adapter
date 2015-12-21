4722c1824307793b71f8242f83dc1ae236f25826
Javier Arnáiz <javier.arnaiz@upplication.com>
2015-12-21 18:22:59 +0100

    - improved serialization attrs in button adapter

--- src/types/button.js
+++ src/types/button.js
@@ -12,6 +12,9 @@ ButtonAdapter = {
 		$a.html(data.text);
 		$a.attr('href', data.href);
 		$a.attr('data-st-user-href', data['user-href']);
+		// we dont use $a.css('style') because css method use computed style and could change all the style attr of the elem.
+		$a.attr('style', 'overflow: hidden; display: block; line-height: normal; box-sizing: border-box; border-style: solid; text-align: center; margin: 0 auto;');
+		$a.find('> *').css('margin', '0');
 
 		Object.keys(data).forEach(function(e) {
 			if (!/^css\-/.test(e))
@@ -19,21 +22,11 @@ ButtonAdapter = {
 			var cssKey = e.replace(/^css-/, '');
 			var cssVal = data[e];
 			$a.attr('style', $a.attr('style') + cssKey + ':' + cssVal + ';');
-			$a.css(cssKey, cssVal);
 		});
-
-		$a.css('overflow', 'hidden');
-		$a.css('display', 'block');
-		$a.css('line-height', 'normal');
-		$a.css('box-sizing', 'border-box');
-		$a.css('border-style', 'solid');
-		$a.css('text-align', 'center');
-		$a.css('margin', '0 auto');
-		$a.find('> *').css('margin', '0');
 
 		return $div.html();
 	},
-	
+
 	toJSON: function(html) {
 		var $a = $(html);
 
@@ -42,12 +35,16 @@ ButtonAdapter = {
 		data.text = $a.html();
 		data.href = $a.attr('href');
 		data['user-href'] = $a.attr('data-st-user-href');
-		$a.attr('style').split(';').forEach(function (e) {
-			var v = e.replace(/\s/g, '').split(':');
-			var cssAttr = v[0];
-			var cssVal = v[1];
-			data['css-' + cssAttr] = cssVal;
-		});
+		$a.attr('style').split(';')
+			.filter(function(elem) {
+				return elem.indexOf(':') != -1
+			})
+			.forEach(function (e) {
+				var v = e.split(':');
+				var cssAttr = v[0].replace(/\s/g, '');
+				var cssVal = v[1].trim();
+				data['css-' + cssAttr] = cssVal;
+			});
 
 		return data;
 	}
