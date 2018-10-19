93450581a16abae85613b0a6e0e1611747578d53
Javier Arnáiz <arnaix@gmail.com>
2018-10-19 13:42:15 +0200

    bump package version

--- sir-trevor-adapter.js
+++ sir-trevor-adapter.js
@@ -1288,7 +1288,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	    ],
 
 	    toHTML: function(data) {
-	        var img_src = _template("https://maps.googleapis.com/maps/api/staticmap?size=<%= width %>x<%= height %>&center=<%= address %>&markers=|<%= address %>&zoom=<%= zoom %>&scale=2")(data);
+	        var img_src = _template("https://maps.googleapis.com/maps/api/staticmap?size=<%= width %>x<%= height %>&center=<%= address %>&markers=|<%= address %>&zoom=<%= zoom %>&scale=2&key=<%= key %>")(data);
 	        var map_ref = _template("http://maps.google.com/maps?q=<%= address %>")(data);
 	        var template = '<a href="<%= map_ref %>"><img src="<%= img_src %>" /></a>';
 	        return _template(template)({ img_src: img_src, map_ref: map_ref });
